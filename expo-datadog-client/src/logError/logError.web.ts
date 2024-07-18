0fe41218d140160443fd944c69159da1e2e635d9
Greg Westneat <greg.westneat@gmail.com>
2024-07-17 19:08:34 -0700

    fixup log error

--- expo-datadog-client/src/logError/logError.web.ts
+++ expo-datadog-client/src/logError/logError.web.ts
@@ -2,10 +2,13 @@ import { datadogRum } from '@datadog/browser-rum';
 
 import type { LogError } from './logError.types';
 
-// simple alias is all that's needed for web
 export const logError: LogError = async (...passThruArgs) => {
-  return new Promise((resolve) => {
-    datadogRum.addError(...passThruArgs);
-    resolve();
-  });
+  const [error, context] = passThruArgs;
+
+  if (typeof context === 'object') {
+    datadogRum.addError(error, context);
+    return;
+  }
+
+  datadogRum.addError(error);
 };
